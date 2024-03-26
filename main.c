c4095f148486d61876f7e1a6ff8e5f5648f53278
smngvlkz <asmdigital14@gmail.com>
2024-03-26 03:56:23 -0700

    fixed a few spelling errors

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	FILE *file;
 	char *line = NULL;
 	size_t len = 0;
-	size_t read;
+	ssize_t read;
 	unsigned int line_number = 0;
 	char *opcode;
 	stack_t *stack = NULL;
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
+i
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	while ((read = getline(&lne, &len, file)) != -1)
+	while ((read = getline(&line, &len, file)) != -1)
 	{
 		line_number++;
 		opcode = strtok(line, "\n\t\r ");
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_numger, opcode);
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 			free(line);
 			fclose(file);
 			exit(EXIT_FAILURE);
