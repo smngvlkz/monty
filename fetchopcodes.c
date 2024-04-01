0ff021e6236d2746ef869ca0022acbb0fba91e56
smngvlkz <asmdigital14@gmail.com>
2024-04-01 01:05:14 -0700

    Fix indentation issues to complywith Betty style guidelines

--- fetchopcodes.c
+++ fetchopcodes.c
@@ -11,30 +11,29 @@
 void (*fetch_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 {
 	instruction_t instruct[] = {
-        {"push", _push},
-        {"pall", pall_it},
-        {"pint", _pint},
-        {"pop", _pop},
-        {"swap", _swap},
-        {"add", add_it},
-        {"sub", sub_it},
-        {"mul", mul_it},
-        {"div", div_it},
-        {"mod", mod_it},
-        {"nop", nop_do},
-        {"stack", stack_set},
-        {"queue", queue_set},
-	{"rotl", my_rotl},
-	{"rotr", my_rotr},
-	{"pchar", pchar},
-	{"pstr", pstr},
-        {NULL, NULL}
+		{"push", _push},
+		{"pall", pall_it},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", add_it},
+		{"sub", sub_it},
+		{"mul", mul_it},
+		{"div", div_it},
+		{"mod", mod_it},
+		{"nop", nop_do},
+		{"stack", stack_set},
+		{"queue", queue_set},
+		{"rotl", my_rotl},
+		{"rotr", my_rotr},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{NULL, NULL}
 	};
 	int i;
 
 	for (i = 0; instruct[i].opcode; i++)
 	{
-
 		if (strcmp(instruct[i].opcode, opc) == 0)
 			return (instruct[i].f);
 	}
